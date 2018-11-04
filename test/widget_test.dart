9a60887fa4ecb0891f2c20d7a80049c6020c1e5d
Masayuki Ouchi <ouchi.masayuki.0717@gmail.com>
2018-11-04 16:50:54 +0900

    リスト2-8 ウィジェットに対するテストを書いてみる

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_app_intro/main.dart';
@@ -6,5 +7,8 @@ void main() {
   testWidgets('flutter_app_intro sample test!', (WidgetTester tester) async {
     await tester.pumpWidget(new MyApp());
     await tester.pump();
+
+    expect(find.text('0'), findsOneWidget);
+    expect(find.byIcon(Icons.add), findsOneWidget);
   });
 }
