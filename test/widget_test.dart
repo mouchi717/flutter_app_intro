33d7b8d66ffbd49c8df812b1c5493682de325789
Masayuki Ouchi <ouchi.masayuki.0717@gmail.com>
2018-11-04 16:56:53 +0900

    リスト2-9 改めてデフォルトのテストコードを確認する

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,11 +4,20 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_app_intro/main.dart';
 
 void main() {
-  testWidgets('flutter_app_intro sample test!', (WidgetTester tester) async {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger frame.
     await tester.pumpWidget(new MyApp());
+
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
-    expect(find.text('0'), findsOneWidget);
-    expect(find.byIcon(Icons.add), findsOneWidget);
+    // Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
   });
 }
