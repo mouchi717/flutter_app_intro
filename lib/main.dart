15d538fd871aec914f9c3a93e7fb7104214188df
Masayuki Ouchi <ouchi.masayuki.0717@gmail.com>
2018-11-04 22:16:21 +0900

    リスト3-1 flutterstudio.appで作成したレイアウト

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_intro/3-1/main.dart';
 
-void main() => runApp(new MyApp());
+
+void main() => runApp(new MyApp31());
 
 class MyApp extends StatelessWidget {
 
