c2b022fc97d4b61be200afea01e01114db7aa74e
Masayuki Ouchi <ouchi.masayuki.0717@gmail.com>
2018-11-04 16:01:10 +0900

    リスト2-3 StatefulsWidgetを使ってみた

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,44 @@ import 'package:flutter/material.dart';
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  final title = 'Flutterサンプル';
+  final message = 'サンプル・メッセージ。';
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Flutter Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello Flutter!'),
-        ),
-        body: Text(
-            'Hello Flutter World!!',
-            style: TextStyle(fontSize: 32.0),
-        ),
+      home: MyHomePage(
+        title: this.title,
+        message: this.message
+      )
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  final String message;
+
+  MyHomePage({this.title, this.message}): super();
+
+  @override
+  _MyHomePageState createState()  => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // インスタンス生成時に必ず呼び出される
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Text(
+        widget.message,
+        style: TextStyle(fontSize: 32.0),
       ),
     );
   }
 }
+
