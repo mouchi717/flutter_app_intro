b1eb32c98b6b5366f921c06dba59b5f0ca961705
Masayuki Ouchi <ouchi.masayuki.0717@gmail.com>
2018-12-03 23:06:27 +0900

    リスト6-4 テキストの描画

--- lib/6-1/main.dart
+++ lib/6-1/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
+import 'dart:ui' as ui;
 
 class MyApp extends StatelessWidget {
 
@@ -63,17 +64,20 @@ class _MyRenderBox extends RenderBox {
     int dx = offset.dx.toInt();
     int dy = offset.dy.toInt();
 
-    Paint p = Paint();
-    p.style = PaintingStyle.stroke;
-    p.strokeWidth = 5.0;
-    p.color = Color.fromARGB(150, 0, 200, 255);
-    for (var i = 0; i <= 10; i++) {
-      Rect r = Rect.fromLTRB(
-          dx + 50.0 + 20 * i,
-          dy + 50.0,
-          dx + 50.0,
-          dy + 250.0 - 20 * i);
-      c.drawLine(r.topLeft, r.bottomRight, p);
-    }
+    ui.ParagraphBuilder builder = ui.ParagraphBuilder(
+        ui.ParagraphStyle(textDirection: TextDirection.ltr),
+      )
+      ..pushStyle(ui.TextStyle(color: Colors.red, fontSize: 48.0))
+      ..addText('Hello! ')
+      ..pushStyle(ui.TextStyle(color: Colors.blue[700], fontSize: 30.0))
+      ..addText('This is a sample of paragraph text. ')
+      ..pushStyle(ui.TextStyle(color: Colors.blue[200], fontSize: 30.0))
+      ..addText('You can draw MULTI-FONT text!');
+
+    ui.Paragraph paragraph = builder.build()
+      ..layout(ui.ParagraphConstraints(width: 300.0));
+
+    Offset off = Offset(dx + 50.0, dy + 50.0);
+    c.drawParagraph(paragraph, off);
   }
 }
